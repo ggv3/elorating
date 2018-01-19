38e6e2f6caa0c608c06e0fd836fb55498fcf11d8
Niko Kaartinen <netkaartinen@gmail.com>
2018-01-19 19:44:13 +0200

    Initial commit

---
+++ src/main/java/fi/kaamos/elorating/EloratingApplication.java
@@ -0,0 +1,12 @@
+package fi.kaamos.elorating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EloratingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EloratingApplication.class, args);
+	}
+}
