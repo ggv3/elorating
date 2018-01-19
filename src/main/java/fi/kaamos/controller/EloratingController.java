33b22736c87ee8e9f182200f773325ad0c8226b8
Niko Kaartinen <netkaartinen@gmail.com>
2018-01-20 00:55:29 +0200

    Add /add endpoint where you can add new players

--- src/main/java/fi/kaamos/controller/EloratingController.java
+++ src/main/java/fi/kaamos/controller/EloratingController.java
@@ -1,15 +1,34 @@
 package fi.kaamos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fi.kaamos.bean.Player;
+import fi.kaamos.bean.PlayerRepository;
+
 @RestController
 public class EloratingController {
+	
+	@Autowired
+	private PlayerRepository playerReposiroty;
 
 	@RequestMapping("/help")
 	public @ResponseBody String help() {
 		String help = "foo";
 		return help;
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public @ResponseBody String addNew(@RequestParam String username) {
+		Player player = new Player();
+		player.setUsername(username);
+		player.setScore(0);
+		player.setPlaycount(0);
+		playerReposiroty.save(player);
+		return "Player " + player.getUsername() + " added!";
 	}
 }
